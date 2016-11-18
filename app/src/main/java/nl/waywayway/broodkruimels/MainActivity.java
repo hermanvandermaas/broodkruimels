435893e2bdb996460399a99a408abba4250c3c97
Herman van der Maas <herman@waywayway.nl>
2016-11-18 16:15:47 +0100

    Netwerkverbinding check

--- app/src/main/java/nl/waywayway/broodkruimels/MainActivity.java
+++ app/src/main/java/nl/waywayway/broodkruimels/MainActivity.java
@@ -1,15 +1,12 @@
 package nl.waywayway.broodkruimels;
 
+import android.content.*;
+import android.net.*;
 import android.os.*;
 import android.support.design.widget.*;
-import android.support.v4.widget.*;
 import android.support.v7.app.*;
 import android.support.v7.widget.*;
 import android.view.*;
-import android.widget.*;
-import nl.waywayway.broodkruimels.*;
-
-import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity 
 {
@@ -17,35 +14,77 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-		
+        setContentView(R.layout.activity_main);
+
+		// Maak toolbar
 		Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
 		setSupportActionBar(toolbar);
-		
-		ActionBar actionBar = getSupportActionBar();		
+		ActionBar actionBar = getSupportActionBar();
+
     }
-	
+
+	// Maak options menu in toolbar
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    public boolean onCreateOptionsMenu(Menu menu)
+	{
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
+    public boolean onOptionsItemSelected(MenuItem item)
+	{
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        switch (id) {
+        switch (id)
+		{
             case R.id.action_settings:
                 return true;
         }
 
         return super.onOptionsItemSelected(item);
     }
+
+	// LinearLayout mProgress = (LinearLayout) findViewById(R.id.LinLayProgress);
+
+	// Als verbinding, download xml
+	// als geen verbinding, toon boodschap met knop probeer opnieuw
+	private void initialize()
+	{
+		if (isNetworkConnected())
+		{
+			downloadXml();
+		} 
+		else
+		{
+			tryAgain();
+		}
+	}
 	
 	
+	// Netwerkverbinding ja/nee
+	private boolean isNetworkConnected()
+	{
+		ConnectivityManager connMgr = (ConnectivityManager)
+			getSystemService(Context.CONNECTIVITY_SERVICE);
+		NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+		return networkInfo != null && networkInfo.isConnected();
+	}
+
+
+	// Download xml
+	private void downloadXml()
+	{
+
+	}
+
+	// Toon boodschap geen verbinding, knop voor opnieuw proberen
+	private void tryAgain()
+	{
+		
+	}	
 }
