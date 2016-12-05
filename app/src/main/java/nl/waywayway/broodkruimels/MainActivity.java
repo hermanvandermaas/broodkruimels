85d2f473a01f293de390930571f35ccfea660472
Herman van der Maas <herman@waywayway.nl>
2016-12-05 17:33:36 +0100

    Taskfragment werkt

--- app/src/main/java/nl/waywayway/broodkruimels/MainActivity.java
+++ app/src/main/java/nl/waywayway/broodkruimels/MainActivity.java
@@ -4,12 +4,24 @@ import android.content.*;
 import android.net.*;
 import android.os.*;
 import android.support.design.widget.*;
+import android.support.v4.app.*;
 import android.support.v7.app.*;
 import android.support.v7.widget.*;
+import android.util.*;
 import android.view.*;
+import android.widget.*;
 
-public class MainActivity extends AppCompatActivity 
+import android.support.v7.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity implements TaskFragment.TaskCallbacks
 {
+	private static final String TAG = MainActivity.class.getSimpleName();
+	private static final boolean DEBUG = true; // Set this to false to disable logs.
+	private static final String KEY_CURRENT_PROGRESS = "current_progress";
+	private static final String KEY_PERCENT_PROGRESS = "percent_progress";
+	private static final String TAG_TASK_FRAGMENT = "task_fragment";
+	private TaskFragment mTaskFragment;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -21,17 +33,19 @@ public class MainActivity extends AppCompatActivity
 		setSupportActionBar(toolbar);
 		ActionBar actionBar = getSupportActionBar();
 
-		// Als verbinding, download xml
-		// als geen verbinding, toon boodschap met knop probeer opnieuw
-		if (isNetworkConnected())
-		{
-			// downloadXml();
-		} 
-		else
-		{
-			// tryAgain();
-		}
+		FragmentManager fm = getSupportFragmentManager();
+		mTaskFragment = (TaskFragment) fm.findFragmentByTag(TAG_TASK_FRAGMENT);
 
+		// If the Fragment is non-null, then it is being retained
+		// over a configuration change.
+		if (mTaskFragment == null)
+		{
+			mTaskFragment = new TaskFragment();
+			fm.beginTransaction().add(mTaskFragment, TAG_TASK_FRAGMENT).commit();
+		}
+		
+		// Controle netwerkverbinding en
+		// download xml in methode onStart() onderaan
     }
 
 	// Maak options menu in toolbar
@@ -75,4 +89,93 @@ public class MainActivity extends AppCompatActivity
 		mSnackbar.show();
 	}
 
+	@Override
+	public void onSaveInstanceState(Bundle outState)
+	{
+		if (DEBUG) Log.i(TAG, "onSaveInstanceState(Bundle)");
+		super.onSaveInstanceState(outState);
+	}
+
+	/*********************************/
+	/***** TASK CALLBACK METHODS *****/
+	/*********************************/
+
+	@Override
+	public void onPreExecute()
+	{
+		if (DEBUG) Log.i(TAG, "onPreExecute()");
+		Toast.makeText(this, "onPreExecute", Toast.LENGTH_SHORT).show();
+	}
+
+	@Override
+	public void onProgressUpdate(int percent)
+	{
+		if (DEBUG) Log.i(TAG, "onProgressUpdate(" + percent + "%)");
+	}
+
+	@Override
+	public void onCancelled()
+	{
+		if (DEBUG) Log.i(TAG, "onCancelled()");
+		Toast.makeText(this, "onCancelled", Toast.LENGTH_SHORT).show();
+	}
+
+	@Override
+	public void onPostExecute()
+	{
+		if (DEBUG) Log.i(TAG, "onPostExecute()");
+		Toast.makeText(this, "onPostExecute", Toast.LENGTH_SHORT).show();
+	}
+
+	/************************/
+	/***** LOGS & STUFF *****/
+	/************************/
+
+	@Override
+	protected void onStart()
+	{
+		if (DEBUG) Log.i(TAG, "onStart()");
+		super.onStart();
+		
+		// Als verbinding, download xml
+		// als geen verbinding, toon boodschap met knop probeer opnieuw
+		if ( isNetworkConnected() )
+		{
+			// Start asynchrone taak
+			if ( !mTaskFragment.isRunning() )
+				mTaskFragment.start();
+		} 
+		else
+		{
+			// TODO: probeer opnieuw
+		}
+	}
+
+	@Override
+	protected void onResume()
+	{
+		if (DEBUG) Log.i(TAG, "onResume()");
+		super.onResume();
+	}
+
+	@Override
+	protected void onPause()
+	{
+		if (DEBUG) Log.i(TAG, "onPause()");
+		super.onPause();
+	}
+
+	@Override
+	protected void onStop()
+	{
+		if (DEBUG) Log.i(TAG, "onStop()");
+		super.onStop();
+	}
+
+	@Override
+	protected void onDestroy()
+	{
+		if (DEBUG) Log.i(TAG, "onDestroy()");
+		super.onDestroy();
+	}
 }
