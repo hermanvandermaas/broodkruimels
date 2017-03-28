924b9ecc9ba624aa09efc1f13cb1c364211a30b3
Herman van der Maas <herman@waywayway.nl>
2017-03-28 08:22:08 +0200

    [add] dialogpreference crasht nog, zonder log...

--- app/src/main/java/nl/waywayway/broodkruimels/SettingsFragment.java
+++ app/src/main/java/nl/waywayway/broodkruimels/SettingsFragment.java
@@ -2,6 +2,7 @@ package nl.waywayway.broodkruimels;
 
 import android.content.*;
 import android.os.*;
+import android.support.v4.app.*;
 import android.support.v7.preference.*;
 import android.util.*;
 
@@ -95,6 +96,35 @@ public class SettingsFragment extends PreferenceFragmentCompat implements Shared
 			setPrefNotifySummary(key);
 		}
 	}
+
+	@Override
+    public void onDisplayPreferenceDialog(Preference preference)
+	{
+        // Try if the preference is one of our custom Preferences
+        DialogFragment dialogFragment = null;
+
+        if (preference instanceof TimePreference)
+		{
+            // Create a new instance of TimePreferenceDialogFragment with the key of the related
+            // Preference
+            dialogFragment = TimePreferenceDialogFragmentCompat.newInstance(preference.getKey());
+        }
+
+
+        if (dialogFragment != null)
+		{
+            // The dialog was created (it was one of our custom Preferences), show the dialog for it
+            dialogFragment.setTargetFragment(this, 0);
+            dialogFragment.show(this.getFragmentManager(), "android.support.v7.preference" +
+								".PreferenceFragment.DIALOG");
+        }
+		else
+		{
+            // Dialog creation could not be handled here. Try with the super method.
+            super.onDisplayPreferenceDialog(preference);
+        }
+
+    }
 }
 
 
--- app/src/main/java/nl/waywayway/broodkruimels/TimePreference.java
+++ app/src/main/java/nl/waywayway/broodkruimels/TimePreference.java
@@ -1,13 +1,22 @@
 package nl.waywayway.broodkruimels;
 
 import android.content.*;
+import android.content.res.*;
 import android.support.v7.preference.*;
 import android.util.*;
 
 public class TimePreference extends DialogPreference
 {
-	private  int  mTime;
-	private  int  mDialogLayoutResId  =  R.layout.preferences_timepicker_dialog;
+	// De tijd in hele minuten na middernacht
+	private int mTime;
+
+	/**
+     * Resource of the dialog layout
+     */
+    private int mDialogLayoutResId = R.layout.preferences_dialog_timepicker;
+	
+	// 4 constructors voor verschillende API levels,
+	// die elkaar in volgorde aanroepen, beginnend met de constructor met de minste parameters
 
 	public TimePreference(Context context)
 	{
@@ -16,7 +25,7 @@ public class TimePreference extends DialogPreference
 
 	public TimePreference(Context context, AttributeSet attrs)
 	{
-		this(context, attrs, 0);
+		this(context, attrs, R.attr.preferenceStyle);
 	}
 
 	public  TimePreference(Context context, AttributeSet attrs, int defStyleAttr)
@@ -28,7 +37,8 @@ public class TimePreference extends DialogPreference
 	{
 		super(context, attrs, defStyleAttr, defStyleRes);
 	}
-
+	
+	// Getter en setter voor de tijd
 	public int getTime()
 	{
 		return  mTime;
@@ -37,8 +47,28 @@ public class TimePreference extends DialogPreference
 	public void setTime(int time)
 	{
 		mTime = time;
-		
+
 		// Save to Shared Preferences
 		persistInt(time);
+	}
+
+	@Override
+    public int getDialogLayoutResource() {
+        return mDialogLayoutResId;
+    }
+	
+	@Override
+	protected Object onGetDefaultValue(TypedArray a, int index)
+	{
+		//  Default  value  from  attribute.  Fallback  value  is  set  to  0.
+		return a.getInt(index,  0);
+	}
+	
+	@Override
+	protected void onSetInitialValue(boolean restorePersistedValue, Object defaultValue)
+	{
+		// Read the value. Use the default value if it is not possible.
+		setTime(restorePersistedValue ?
+				getPersistedInt(mTime) : (int) defaultValue);
 	}
 }
