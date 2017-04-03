42505f5a0a9169ec5f37e9400cabeb128fcafc64
Herman van der Maas <herman@waywayway.nl>
2017-04-03 17:37:26 +0200

    [change] Timepreference verwijderd

--- app/src/main/java/nl/waywayway/broodkruimels/SettingsFragment.java
+++ app/src/main/java/nl/waywayway/broodkruimels/SettingsFragment.java
@@ -91,40 +91,11 @@ public class SettingsFragment extends PreferenceFragmentCompat implements Shared
 	// Bij verandering in instelling, geef de nieuwe instelling direct weer in de summary
 	public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) 
 	{
-		if (key == SettingsActivity.KEY_PREF_NOTIFY)
+		if (key.equals(SettingsActivity.KEY_PREF_NOTIFY))
 		{
 			setPrefNotifySummary(key);
 		}
 	}
-
-	@Override
-    public void onDisplayPreferenceDialog(Preference preference)
-	{
-        // Try if the preference is one of our custom Preferences
-        DialogFragment dialogFragment = null;
-
-        if (preference instanceof TimePreference)
-		{
-            // Create a new instance of TimePreferenceDialogFragment with the key of the related
-            // Preference
-            dialogFragment = TimePreferenceDialogFragmentCompat.newInstance(preference.getKey());
-        }
-
-
-        if (dialogFragment != null)
-		{
-            // The dialog was created (it was one of our custom Preferences), show the dialog for it
-            dialogFragment.setTargetFragment(this, 0);
-            dialogFragment.show(this.getFragmentManager(), "android.support.v7.preference" +
-								".PreferenceFragment.DIALOG");
-        }
-		else
-		{
-            // Dialog creation could not be handled here. Try with the super method.
-            super.onDisplayPreferenceDialog(preference);
-        }
-
-    }
 }
 
 
