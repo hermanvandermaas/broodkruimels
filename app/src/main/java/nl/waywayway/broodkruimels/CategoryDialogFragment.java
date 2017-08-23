6295eb24b0255332797c7c2945feee0c5ccdc7f6
Herman van der Maas <herman@waywayway.nl>
2017-08-23 20:55:36 +0200

    fix saveInstanceState

--- app/src/main/java/nl/waywayway/broodkruimels/CategoryDialogFragment.java
+++ app/src/main/java/nl/waywayway/broodkruimels/CategoryDialogFragment.java
@@ -16,65 +16,85 @@ public class CategoryDialogFragment extends DialogFragment
 	private List<CategoryItem> categoryList;
 	private String[] categoryArray;
 	private ArrayList mSelectedItems;
-	
+
 	public void setCategoryList(List<CategoryItem> categoryList)
 	{
 		this.categoryList = categoryList;
 	}
-	
+
 	@Override
 	public Dialog onCreateDialog(Bundle savedInstanceState)
 	{
-		// categoryArray = new String[]{"Kruipen", "Lopen", "Rennen"};
-		Log.i("HermLog", "categoryList is null? " + (categoryList == null));
-		categoryArray = makeCategoryArray((ArrayList<CategoryItem>) categoryList);
+		if (savedInstanceState != null)
+		{
+			super.onCreateDialog(savedInstanceState);
+			categoryArray = savedInstanceState.getStringArray("savedCategoryArray");
+		}
+		else
+		{
+			// categoryArray = new String[]{"Kruipen", "Lopen", "Rennen"};
+			Log.i("HermLog", "categoryList is null? " + (categoryList == null));
+			categoryArray = makeCategoryArray((ArrayList<CategoryItem>) categoryList);
+		}
+
 		mSelectedItems = new ArrayList();
-		
+
 		AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
 		builder
 			.setTitle(R.string.dialog_category_title)
 			.setMultiChoiceItems(categoryArray, null,
-				new DialogInterface.OnMultiChoiceClickListener()
+			new DialogInterface.OnMultiChoiceClickListener()
+			{
+				@Override
+				public void onClick(DialogInterface dialog, int which, boolean isChecked)
 				{
-					@Override
-					public void onClick(DialogInterface dialog, int which, boolean isChecked)
+					if (isChecked)
 					{
-						if (isChecked)
-						{
-							// If the user checked the item, add it to the selected items
-							mSelectedItems.add(which);
-						}
-						else if (mSelectedItems.contains(which))
-						{
-							// Else, if the item is already in the array, remove it
-							mSelectedItems.remove(Integer.valueOf(which));
-						}
+						// If the user checked the item, add it to the selected items
+						mSelectedItems.add(which);
 					}
-				})
+					else if (mSelectedItems.contains(which))
+					{
+						// Else, if the item is already in the array, remove it
+						mSelectedItems.remove(Integer.valueOf(which));
+					}
+				}
+			})
 			.setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener()
+			{
+				public void onClick(DialogInterface dialog, int id)
 				{
-					public void onClick(DialogInterface dialog, int id)
-					{
-						// FIRE ZE MISSILES!
-					}
-				})
+					// FIRE ZE MISSILES!
+				}
+			})
 			.setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener()
+			{
+				public void onClick(DialogInterface dialog, int id)
 				{
-					public void onClick(DialogInterface dialog, int id)
-					{
-						// User cancelled the dialog
-					}
-				});
-				
+					// User cancelled the dialog
+				}
+			});
+
 		return builder.create();
 	}
-	
+
+	@Override
+	public void onSaveInstanceState(Bundle outState)
+	{
+		super.onSaveInstanceState(outState);
+
+		if (categoryArray != null)
+		{
+			outState.putStringArray("savedCategoryArray", categoryArray);
+		}
+	}
+
 	private String[] makeCategoryArray(ArrayList<CategoryItem> categoryList)
 	{
 		// Maak ArrayList<String> van ArrayList<CategoryItem>
 		// maak daarna String[] van ArrayList<String>
 		ArrayList<String> categoryArrayList = new ArrayList<String>();
-		
+
 		for (CategoryItem item : categoryList)
 		{
 			String name = item.getName();
@@ -82,11 +102,11 @@ public class CategoryDialogFragment extends DialogFragment
 			Log.i("HermLog", "categoryArrayList is null? " + (categoryArrayList == null));
 			categoryArrayList.add(name);
 		}
-		
+
 		Log.i("HermLog", "categoryArrayList is null? " + (categoryArrayList == null));
 		Log.i("HermLog", "categoryArray is null? " + (categoryArray == null));
 		Log.i("HermLog", "(new String[0]) is null? " + ((new String[0]) == null));
-		
+
 		categoryArray = categoryArrayList.toArray(new String[0]);
 		return categoryArray;
 	}
